1da02b9ff19132d73a5f4e4df2c692b36ff958cd
garyb6 <gary.bennett6@outlook.com>
2021-11-22 16:56:48 +0000

    Hannahhelp

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -18,52 +18,27 @@ def increase_pets_sold(pet_shop, amount): ##this will be similar to those above,
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"]) 
 
-# def get_pets_by_breed(pet_shop, pet_breed): 
-#     number_of_breed = []
-#     for pet in pet_shop["pets"]:
-#         if pet["breed"] == pet_breed:
-#             number_of_breed.append(pet) 
-#     return len(number_of_breed)
-
 def get_pets_by_breed(pet_shop, pet_breed): 
-    number_of_breed = 0
+    found_pets = []
     for pet in pet_shop["pets"]:
         if pet["breed"] == pet_breed:
-            number_of_breed += 1
-    return number_of_breed
+            found_pets.append(pet) 
+    return found_pets
+
+# def get_pets_by_breed(pet_shop, pet_breed): 
+#     number_of_breed = 0
+#     for pet in pet_shop["pets"]:
+#         if pet["breed"] == pet_breed:
+#             number_of_breed += 1
+#     return number_of_breed
 
 
-# def find_pet_by_name(pet, pet_shop): #I get what I need to do but I really can't figure out how to access the cc_pet_shop (dictionary), "into pets" (list) and then access the dictionary key "name" to see if the value matches Arthur
-#     found_pet = None 
-
-#     for pet in pet_shop:
-#         if pet("pets"["name"]) == "Arthur":
-#             found_pet = pet 
-#     return found_pet 
-
-# def find_chicken_by_name(list, chicken_name):
-#     found_chicken = None
-#     for chicken in list:
-#         if chicken ["name"] == chicken_name:
-#             found_chicken = chicken 
-# 
-#     return found_chicken 
-# 
-# def find_record_by_title (title, record_store):
-#     found_record = None #in case nothing is found
-# 
-#     for record in record_store ["records"]: #for dictionary item in key of dictionary
-#         if record["title"] == title: #does the title passing in match dictionary items
-#             found_record = record #if true, return it. if not, return None. 
-# 
-#     return found_record 
-
-# def find_pet_by_name (pet, pet_shop):
-#     pet_name = None 
-#     for pet in pet_shop["pets"]: #TypeError: string indices must be integers
-#         if pet["name"] == "Arthur":
-#             pet_name = pet 
-#     return pet_name 
+def find_pet_by_name(pet_shop, pet_name):
+    found_pet = None 
+    for pet in pet_shop["pets"]: #accessing the dictionary and then the list within the dictionary - for examnple cc_pet_shop (dict) "pets" (list)
+        if pet["name"] == pet_name:
+            found_pet = pet 
+    return found_pet 
 
 
 def remove_pet_by_name ():
